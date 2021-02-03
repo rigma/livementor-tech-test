a11cc078c1048fbd9cba5bb9236bea12e0ed51f4
rigma <rigbuntu@gmail.com>
2021-02-03 16:15:34 +0100

    add class methods to load json strings and files

--- lib/livementor_tech_test/json_translator.rb
+++ lib/livementor_tech_test/json_translator.rb
@@ -3,5 +3,28 @@ require 'json'
 
 module LiveMentorTechTest
   class JsonTranslator
+    attr_reader :doc
+
+    def initialize(doc)
+      @doc = doc
+    end
+
+    def headers
+      if @headers == nil
+        @doc.values
+      end
+
+      @headers
+    end
+
+    class << self
+      def from_file(path)
+        JsonTranslator.new(JSON.load_file(path))
+      end
+
+      def from_str(str)
+        JsonTranslator.new(JSON.parse(str))
+      end
+    end
   end
 end
---
+++ spec/livementor_tech_test/json_translator_spec.rb
@@ -0,0 +1,57 @@
+RSpec.describe LiveMentorTechTest::JsonTranslator do
+  it "parses a JSON string" do
+    str = <<EOS
+[
+  {
+    "id": 0,
+    "email": "colleengriffith@quintity.com",
+    "tags": [
+      "consectetur",
+      "quis"
+    ],
+    "profiles": {
+      "facebook": {
+        "id": 0,
+        "picture": "//fbcdn.com/a2244bc1-b10c-4d91-9ce8-184337c6b898.jpg"
+      },
+      "twitter": {
+        "id": 0,
+        "picture": "//twcdn.com/ad9e8cd3-3133-423e-8bbf-0602e4048c22.jpg"
+      }
+    }
+  },
+  {
+    "id": 1,
+    "email": "maryellengriffin@ginkle.com",
+    "tags": [
+      "veniam",
+      "elit",
+      "mollit"
+    ],
+    "profiles": {
+      "facebook": {
+        "id": 1,
+        "picture": "//fbcdn.com/12e070e0-21ea-4663-97d0-46bc9c7b67a4.jpg"
+      },
+      "twitter": {
+        "id": 1,
+        "picture": "//twcdn.com/3057792f-5dfb-4c4b-86b5-cce4d6bbf7ac.jpg"
+      }
+    }
+  }
+]
+EOS
+    translator = LiveMentorTechTest::JsonTranslator.from_str str
+
+    expect(translator.doc.class).to be Array
+    expect(translator.doc[0].class).to be Hash
+  end
+
+  it "parses a JSON file" do
+    translator = LiveMentorTechTest::JsonTranslator.from_file(File.dirname(__FILE__) + "/json_example.json")
+
+    expect(translator.doc.class).to be Array
+    expect(translator.doc[0].class).to be Hash
+    expect(translator.doc[0]["id"]).to eq(0)
+  end
+end
